36d5b4746d67dc58b844b6d05c7b34b3c58da1b6
SUH(green) <suh240505@gmail.com>
2024-04-15 13:58:52 +0900

    성빈 - 광훈버튼이랑 연결해봄(잘됨)

--- storage.js
+++ storage.js
@@ -3,75 +3,69 @@
 
 // 저장
 const save = {
-    calcBefore : {
+    calcBefore: {
         // 광훈오빠 객체 불러오기
-        getBtnData : function(test){
+        getBtnData: function (test) {
             const data = test; // 광훈오빠 객체로부터 데이터를 가져온다고 가정
             this.saveInArr(data); // saveInArr 함수 실행
         },
-        
+
         // data 배열에 저장
-        saveArr : [], // 빈 배열 만들기e : function(){
-            const string = JSON.str
-    
-        saveInArr : function(data){
+        saveArr: [], // 빈 배열 만들기
+
+        saveInArr: function (data) {
             this.saveArr.push(data)
             this.printArr();
             this.saveInLocalStorage();
         },
-        
+
         // test용
-        printArr : function(){
+        printArr: function () {
             console.log(this.saveArr)
         },
-    
-<<<<<<< HEAD
-/*         // 로컬스트리지에 저장하려면 배열->스트링 변화 필요
-        changeTypeToSaveLocalStoragingify(this.saveArr);
-=======
+
         // 로컬스트리지에 저장하려면 배열->스트링 변화 필요
-        changeTypeToSaveLocalStorage : function(){
+        changeTypeToSaveLocalStorage: function () {
             const string = JSON.stringify(this.saveArr);
->>>>>>> 1e8c5463f5becee8d39d2e0e8ff4a3e5f159f6f5
             return string;
         },
-    
+
         // 로컬스토리지 저장
-        saveInLocalStorage : function(){
+        saveInLocalStorage: function () {
             const string = this.changeTypeToSaveLocalStorage();
             window.localStorage.setItem("calcBefore", string);
         }
     },
 
-    calcAfter : { 
+    calcAfter: {
         // 민규오빠 객체 불러오기
-        getCalcData : function(test){
+        getCalcData: function (test) {
             const data = test; // 광훈오빠 객체로부터 데이터를 가져온다고 가정
             this.saveInArr(data); // saveInArr 함수 실행
         },
-        
+
         // data 배열에 저장
-        saveArr : [], // 빈 배열 만들기
-    
-        saveInArr : function(data){
+        saveArr: [], // 빈 배열 만들기
+
+        saveInArr: function (data) {
             this.saveArr.push(data)
             this.printArr();
             this.saveInLocalStorage();
         },
-        
+
         // test용
-        printArr : function(){
+        printArr: function () {
             console.log(this.saveArr)
         },
-    
+
         // 로컬스트리지에 저장하려면 배열->스트링 변화 필요
-        changeTypeToSaveLocalStorage : function(){
+        changeTypeToSaveLocalStorage: function () {
             const string = JSON.stringify(this.saveArr);
             return string;
         },
-    
+
         // 로컬스토리지 저장
-        saveInLocalStorage : function(){
+        saveInLocalStorage: function () {
             const string = this.changeTypeToSaveLocalStorage();
             window.localStorage.setItem("calcAfter", string);
         }
