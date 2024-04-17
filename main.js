6c9fee9ef3ebe800375d0dfcbf0edc3f8aeca1a2
chomin(green) <doaiso381@naver.com>
2024-04-17 16:20:35 +0900

    조민지 - 출력 로컬 스토리지

--- main.js
+++ main.js
@@ -11,14 +11,14 @@ const repeatBtn = function () {
     for (let i = 0; i <= 9; i++) {
         new ButtonTag('', '', '', `${i}`, `${i}`, 'btn');
         const btnNo = document.getElementsByClassName("btn")[i];
-        btnNo.addEventListener("click", function(){ 
+        btnNo.addEventListener("click", function () {
             const value = btnNo.value; // 클릭된 버튼의 값
             const saveCalcBefore = new SaveBefore(value);
-            console.log(saveCalcBefore.saveInArr()) // 버튼 값을 가지고 저장 실행
-            // const printoutPlay = new printout(saveCalcBefore.arr); // 저장 값을 가지고 출력 실행
+            // console.log(saveCalcBefore.saveInArr()) // 버튼 값을 가지고 저장 실행
+            const printoutPlay = new printout(); // 저장 값을 가지고 출력 실행
         });
     };
-} 
+}
 
 new ButtonTag('', '', '', '+', '+', 'operator');
 new ButtonTag('', '', '', '-', '-', 'operator');
@@ -34,32 +34,32 @@ const btnMulti = document.getElementsByClassName('operator')[3];
 const btnClear = document.getElementsByClassName('operator_Clear')[0];
 const btnResult = document.getElementsByClassName('operator_result')[0];
 
-btnPlus.addEventListener("click", function(){ 
+btnPlus.addEventListener("click", function () {
     const value = btnPlus.value; // 클릭된 버튼의 값
     const saveCalcBefore = new SaveBefore(value); // 버튼 값을 가지고 저장 실행
     const printoutPlay = new printout(saveCalcBefore.arr); // 저장 값을 가지고 출력 실행
 });
-btnMinor.addEventListener("click", function(){ 
+btnMinor.addEventListener("click", function () {
     const value = btnMinor.value; // 클릭된 버튼의 값
     const saveCalcBefore = new SaveBefore(value); // 버튼 값을 가지고 저장 실행
     const printoutPlay = new printout(saveCalcBefore.arr); // 저장 값을 가지고 출력 실행
 });
-btnDivide.addEventListener("click", function(){ 
+btnDivide.addEventListener("click", function () {
     const value = btnDivide.value; // 클릭된 버튼의 값
     const saveCalcBefore = new SaveBefore(value); // 버튼 값을 가지고 저장 실행
     const printoutPlay = new printout(saveCalcBefore.arr); // 저장 값을 가지고 출력 실행
 });
-btnMulti.addEventListener("click", function(){ 
+btnMulti.addEventListener("click", function () {
     const value = btnMulti.value; // 클릭된 버튼의 값
     const saveCalcBefore = new SaveBefore(value); // 버튼 값을 가지고 저장 실행
     const printoutPlay = new printout(saveCalcBefore.arr); // 저장 값을 가지고 출력 실행
 });
-btnClear.addEventListener("click", function(){ 
+btnClear.addEventListener("click", function () {
     const value = btnClear.value; // 클릭된 버튼의 값
     const saveCalcBefore = new SaveBefore(value); // 버튼 값을 가지고 저장 실행
     const printoutPlay = new printout(saveCalcBefore.arr); // 저장 값을 가지고 출력 실행
 });
-btnResult.addEventListener("click", function(){ 
+btnResult.addEventListener("click", function () {
     const value = btnResult.value; // 클릭된 버튼의 값
     // const saveCalcBefore = new SaveBefore(value); // 버튼 값을 가지고 저장 실행
     // const printoutPlay = new printout(saveCalcBefore.arr); // 저장 값을 가지고 출력 실행
--- module/printout.js
+++ module/printout.js
@@ -1,20 +1,23 @@
 // 출력 클래스화
 export class printout {
-    constructor(test) {
-        this.test = test;   // this.test 안에 가져온 calcBefore 배열 넣기
-        this.disPrint();    // 디스플레이에 출력하는 메서드 실행
+    constructor() {
+        // this.test = test;   // this.test 안에 가져온 calcBefore 배열 넣기
+        this.beforePrint();    // 디스플레이에 출력하는 메서드 실행
     }
 
     // 배열에서 값 가져오는 메서드
     getNum() {
-        const display = this.test.join('');     // 배열 문자열로 바꿔주기
-        return display;     // 문자열 반환
+        // const display = this.test.join('');     // 배열 문자열로 바꿔주기
+        // return display;     // 문자열 반환
+        const display = localStorage.getItem('calcBefore');
+        return display;
     }
 
     // 디스플레이에 출력하는 메서드
-    disPrint() {
-        const data = this.getNum();     // data 안에 문자열로 바꿔준 값 넣기
-        console.log(`calcBefore : ${data}`);    // 콘솔 창에 출력 -> 나중에 인풋 창에 출력으로 수정해야됨
+    beforePrint() {
+        const bf = this.getNum();     // data 안에 문자열로 바꿔준 값 넣기
+        console.log(`calcBefore : ${bf}`);    // 콘솔 창에 출력 -> 나중에 인풋 창에 출력으로 수정해야됨
+        
 
     }
 }
