2dee3a2237055a68a7cf91f84e20f59de355d8b7
SUH(green) <suh240505@gmail.com>
2024-04-17 16:44:38 +0900

    성빈 - 출력전까지 힘냄

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 import { SaveBefore, SaveAfter } from "./module/save.js";
 import { printout } from "./module/printout.js";
 import { InputTag, ButtonTag } from './module/button_create.js';
-import  Eval  from './module/eval.js';
+import Eval from './module/eval.js';
 
 // 노광훈 작업내역
 new InputTag('', '', '', '', '', '', '', '');
@@ -12,15 +12,15 @@ const repeatBtn = function () {
     for (let i = 0; i <= 9; i++) {
         new ButtonTag('', '', '', `${i}`, `${i}`, 'btn');
         const btnNo = document.getElementsByClassName("btn")[i];
-        btnNo.addEventListener("click", function(){ 
+        btnNo.addEventListener("click", function () {
             const value = btnNo.value; // 클릭된 버튼의 값
             new SaveBefore(value);
             console.log(SaveBefore.arr);
-             // 버튼 값을 가지고 저장 실행
+            // 버튼 값을 가지고 저장 실행
             // const printoutPlay = new printout(saveCalcBefore.arr); // 저장 값을 가지고 출력 실행
         });
     };
-} 
+}
 
 new ButtonTag('', '', '', '+', '+', 'operator');
 new ButtonTag('', '', '', '-', '-', 'operator');
@@ -36,41 +36,46 @@ const btnMulti = document.getElementsByClassName('operator')[3];
 const btnClear = document.getElementsByClassName('operator_Clear')[0];
 const btnResult = document.getElementsByClassName('operator_result')[0];
 
-btnPlus.addEventListener("click", function(){ 
+btnPlus.addEventListener("click", function () {
     const value = btnPlus.value; // 클릭된 버튼의 값
     new SaveBefore(value);
-    console.log(SaveBefore.arr);
+    // console.log(SaveBefore.arr);
 });
-btnMinor.addEventListener("click", function(){ 
+btnMinor.addEventListener("click", function () {
     const value = btnMinor.value; // 클릭된 버튼의 값
     new SaveBefore(value);
-    console.log(SaveBefore.arr);
+    // console.log(SaveBefore.arr);
 });
-btnDivide.addEventListener("click", function(){ 
+btnDivide.addEventListener("click", function () {
     const value = btnDivide.value; // 클릭된 버튼의 값
     new SaveBefore(value);
-    console.log(SaveBefore.arr);
+    // console.log(SaveBefore.arr);
 });
-btnMulti.addEventListener("click", function(){ 
+btnMulti.addEventListener("click", function () {
     const value = btnMulti.value; // 클릭된 버튼의 값
     new SaveBefore(value);
-    console.log(SaveBefore.arr);
+    // console.log(SaveBefore.arr);
 });
-btnClear.addEventListener("click", function(){ 
+btnClear.addEventListener("click", function () {
     const value = btnClear.value; // 클릭된 버튼의 값
     new SaveBefore(value);
-    console.log(SaveBefore.arr);
+    new SaveAfter(value);
+    // console.log(SaveBefore.arr);
 });
-btnResult.addEventListener("click", function(){ 
+btnResult.addEventListener("click", function () {
     // const value = btnResult.value; // 클릭된 버튼의 값
     const calcing = new Eval(); // 버튼 값을 가지고 연산 실행
-    // const saveCalcBefore = new SaveBefore(value); // 버튼 값을 가지고 저장 실행
+    const value = calcing.operator_Cal();
+    new SaveAfter(value);
+    // console.log(SaveAfter.arr);
     // const printoutPlay = new printout(saveCalcBefore.arr); // 저장 값을 가지고 출력 실행
 });
 
 // 노광훈 작업내역
 
 repeatBtn();
+
+
 
 
 // ================우리 코드 방향의 흔적기관 ㅋㅋ=================
