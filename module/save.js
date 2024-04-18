3be334660b02665af6029164503c2e80a90be30c
SUH(green) <suh240505@gmail.com>
2024-04-18 11:06:25 +0900

    성빈 - 전꺼 취소...

--- module/save.js
+++ module/save.js
@@ -6,13 +6,13 @@ class Save {
         this.saveInArr();
         this.saveInLocalStorage();
     }
-    static arr = [];
 }
 
 class SaveBefore extends Save {
     constructor(data) {
         super(data);
     }
+    static arr = [];
 
     saveInArr() {
         if(this.data == " "){
@@ -34,6 +34,7 @@ class SaveAfter extends Save {
     constructor(data) {
         super(data);
     }
+    static arr = [];
 
     saveInArr() {
         if (this.data == " ") {
