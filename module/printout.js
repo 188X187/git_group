f987d8dbde0d7f84605c91214d4198dbc859873f
chomin(green) <doaiso381@naver.com>
2024-04-18 10:06:20 +0900

    조민지 - 출력 모듈 주석 삭제

--- module/printout.js
+++ module/printout.js
@@ -2,7 +2,6 @@
 export class Printout {
     constructor(input) {
         this.test = input;   // this.test 안에 가져온 calcBefore 배열 넣기
-        // this.beforePrint();    // 디스플레이에 출력하는 메서드 실행
     }
 
     // 배열에서 값 가져오는 메서드
@@ -14,7 +13,6 @@ export class Printout {
     // 디스플레이에 출력하는 메서드
     disPrint() {
         const print = this.getNum();     // data 안에 문자열로 바꿔준 값 넣기
-        // console.log(`calcBefore : ${bf}`);    // 콘솔 창에 출력 -> 나중에 인풋 창에 출력으로 수정해야됨
         document.getElementsByName("output")[0].value = print;
     }
 
