d18c0da6bd37d8bfd3970b1b45bce2a2a48ac199
chomin(green) <doaiso381@naver.com>
2024-04-18 15:45:29 +0900

    조민지 - 출력 this 이름 수정

--- module/printout.js
+++ module/printout.js
@@ -1,12 +1,12 @@
 // 출력 클래스화
 export class Printout {
     constructor(input) {
-        this.test = input;   // this.test 안에 가져온 calcBefore 배열 넣기
+        this.input = input;   // this.test 안에 가져온 배열 넣기
     }
 
     // 배열에서 값 가져오는 메서드
     getNum() {
-        const display = this.test.join('');     // 배열 문자열로 바꿔주기
+        const display = this.input.join('');     // 배열 문자열로 바꿔주기
         return display;     // 문자열 반환
     }
 
