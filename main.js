89954987e0f2429d66acd408cb87706eb2cb0b67
SUH(green) <suh240505@gmail.com>
2024-04-18 10:05:05 +0900

    성빈 - main 주석 삭제 및 기존 main 프로토타입에 백업

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // main.js
-import { SaveBefore, SaveAfter } from "./module/save.js";
-import { Printout } from "./module/printout.js";
 import { InputTag, ButtonTag } from './module/button.js';
+import { SaveBefore, SaveAfter } from "./module/save.js";
 import Eval from './module/eval.js';
+import { Printout } from "./module/printout.js";
 
 // Button 생성
 new InputTag('', '', '', '', '', '', '', '');
@@ -14,15 +14,11 @@ const repeatBtn = function () {
         btnNo.addEventListener("click", function () {
             const value = btnNo.value; // 클릭된 버튼의 값
             new SaveBefore(value);
-            // console.log(SaveBefore.arr);
-            // 버튼 값을 가지고 저장 실행
             const printoutPlay = new Printout(SaveBefore.arr); // 저장 값을 가지고 출력 실행
             printoutPlay.disPrint();
         });
     };
 }
-
-
 
 new ButtonTag('', '', '', '+', '+', 'operator');
 new ButtonTag('', '', '', '-', '-', 'operator');
@@ -31,13 +27,8 @@ new ButtonTag('', '', '', '*', '*', 'operator');
 new ButtonTag('', '', '', ' ', 'AC', 'operator_Clear');
 new ButtonTag('', '', '', '=', '=', 'operator_result');
 
-// const btnPlus = document.getElementsByClassName('operator')[0];
-// const btnMinor = document.getElementsByClassName('operator')[1];
-// const btnDivide = document.getElementsByClassName('operator')[2];
-// const btnMulti = document.getElementsByClassName('operator')[3];
 const btnClear = document.getElementsByClassName('operator_Clear')[0];
 const btnResult = document.getElementsByClassName('operator_result')[0];
-
 
 // 연산자 버튼 클릭 이벤트
 const opBtn = function () {
@@ -53,35 +44,7 @@ const opBtn = function () {
     }
 }
 
-// 연산자 버튼 클릭 이벤트 흔적..
-// btnPlus.addEventListener("click", function () {
-//     const value = btnPlus.value; // 클릭된 버튼의 값
-//     new SaveBefore(value);
-//     const printoutPlay = new Printout(SaveBefore.arr); // 저장 값을 가지고 출력 실행
-//     printoutPlay.disPrint();
-//     // console.log(SaveBefore.arr);
-// });
-// btnMinor.addEventListener("click", function () {
-//     const value = btnMinor.value; // 클릭된 버튼의 값
-//     new SaveBefore(value);
-//     const printoutPlay = new Printout(SaveBefore.arr); // 저장 값을 가지고 출력 실행
-//     printoutPlay.disPrint();
-//     // console.log(SaveBefore.arr);
-// });
-// btnDivide.addEventListener("click", function () {
-//     const value = btnDivide.value; // 클릭된 버튼의 값
-//     new SaveBefore(value);
-//     const printoutPlay = new Printout(SaveBefore.arr); // 저장 값을 가지고 출력 실행
-//     printoutPlay.disPrint();
-//     // console.log(SaveBefore.arr);
-// });
-// btnMulti.addEventListener("click", function () {
-//     const value = btnMulti.value; // 클릭된 버튼의 값
-//     new SaveBefore(value);
-//     const printoutPlay = new Printout(SaveBefore.arr); // 저장 값을 가지고 출력 실행
-//     printoutPlay.disPrint();
-//     // console.log(SaveBefore.arr);
-// });
+// 연산자 버튼 클릭 
 btnClear.addEventListener("click", function () {
     const value = btnClear.value; // 클릭된 버튼의 값
     new SaveBefore(value);
@@ -90,32 +53,15 @@ btnClear.addEventListener("click", function () {
     printoutPlay.disPrint();
     const printoutPlay2 = new Printout(SaveAfter.arr); // 저장 값을 가지고 출력 실행
     printoutPlay2.disPrint();
-    // console.log(SaveBefore.arr);
 });
+
 btnResult.addEventListener("click", function () {
-    // const value = btnResult.value; // 클릭된 버튼의 값
     const calcing = new Eval(); // 버튼 값을 가지고 연산 실행
     const value = calcing.operator_Cal();
     new SaveAfter(value);
     const printoutPlay2 = new Printout(SaveAfter.arr); // 저장 값을 가지고 출력 실행
     printoutPlay2.disPrint();
-    // console.log(SaveAfter.arr);
-    // const printoutPlay = new printout(saveCalcBefore.arr); // 저장 값을 가지고 출력 실행
 });
-
-// 노광훈 작업내역
 
 repeatBtn();
 opBtn();
-
-
-
-
-// ================우리 코드 방향의 흔적기관 ㅋㅋ=================
-// const btn = "1"
-// const saveCalcBefore = new SaveBefore(btn);
-// // console.log(saveCalcBefore.saveInArr())
-// const printoutPlay = new printout(saveCalcBefore.arr);
-
-// const calc = "3"
-// const saveCalcAfter = new SaveAfter(calc);
